9133c30590092decf462e3ef0ea7204c54b90f3d
lorenzorb <lorenzo.ramosbragg@gmail.com>
2025-11-08 00:16:46 -0800

    Changes to be committed: 	modified:   bike.rb 	modified:   main.rb

--- bike.rb
+++ bike.rb
@@ -5,7 +5,7 @@ class Bike
   STANDARD_WEIGHT = 200 # lbs
   MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight, :rented, :cargo
 
   def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
     @id = id
@@ -13,27 +13,40 @@ class Bike
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
+    @cargo = Cargo.new
   end
 
   def rent!
     self.rented = true
   end
 
+end
+
+
+
+
+
+class Cargo
+  MAX_CARGO_ITEMS = 10
+
+  def initialize(capacity = MAX_CARGO_ITEMS)
+    @capacity = capacity
+    @cargo_contents = []
+  end
+
   def add_cargo(item)
-    self.cargo_contents << item
+    @cargo_contents << item
   end
 
   def remove_cargo(item)
-    self.cargo_contents.remove(item)
+    @cargo_contents.remove(item)
   end
 
-  def pannier_capacity
-    MAX_CARGO_ITEMS
+  def get_capacity
+    @capacity
   end
 
-  def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
+  def get_remaining_capacity
+    @capacity - @cargo_contents.size
   end
-
 end
